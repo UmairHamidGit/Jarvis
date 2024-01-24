e84dc161999d404a3774ef67bf9e97d6a13e41b0
Umair Hamid <153216808+UmairHamidGit@users.noreply.github.com>
2024-01-24 10:11:15 +0530

    Create jarvis.py

---
+++ jarvis.py
@@ -0,0 +1,30 @@
+import pyttsx3
+import datetime
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[0].id)
+engine.setProperty('voice',voices[0].id) 
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon") 
+
+    else:
+        speak("Good Evening")
+
+    speak("I am Jarvis Sir. Please tell me how may I help you")
+if __name__ == "__main__":
+    
+    wishMe()
+

fd45e99632a8601ee13738d5f58e5fb3e609010d
Umair Hamid <153216808+UmairHamidGit@users.noreply.github.com>
2024-02-14 22:32:50 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -1,7 +1,9 @@
-import pyttsx3
-import datetime
 import os
-from gtts import gTTS
+import datetime
+import pyttsx3
+import speech_recognition as sr
+import wikipedia
+import webbrowser
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -19,38 +21,63 @@ def wish_me():
         speak("Good Afternoon!")
     else:
         speak("Good Evening!")
-    speak("I am Jarvis! Sir, Please tell me how may I help you")
+    speak("I am Jarvis! Boss, Please tell me how may I help you")
 
 def take_command():
-    query = input("Enter your command: ").lower()
-    return query
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        recognizer.pause_threshold = 1
+        audio = recognizer.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = recognizer.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+    except Exception as e:
+        print(e)
+        speak("Sorry, I didn't catch that. Can you please repeat?")
+        return "None"
+    return query.lower()
 
 if __name__ == "__main__":
     wish_me()
     while True:
-        query = take_command().lower()
+        query = take_command()
         
         if 'wikipedia' in query:
             speak('Searching Wikipedia...')
             query = query.replace("wikipedia", "")
-            # code for searching Wikipedia and speaking the result
-            
+            try:
+                result = wikipedia.summary(query, sentences=2)
+                speak("According to Wikipedia")
+                speak(result)
+            except wikipedia.exceptions.DisambiguationError as e:
+                speak("There are multiple results for this query. Please be more specific.")
+            except wikipedia.exceptions.PageError as e:
+                speak("Sorry, I couldn't find any relevant information.")
+
         elif 'open youtube' in query:
-            # code for opening YouTube
-            
+            webbrowser.open("https://www.youtube.com")
+
         elif 'open google' in query:
-            # code for opening Google
-            
+            webbrowser.open("https://www.google.com")
+
         elif 'the time' in query:
-            # code for telling the time
-            
+            now = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"The time is {now}")
+
         elif 'quit' in query:
             speak("Goodbye Sir! Jarvis signing off.")
             break
-
         # Add more elif conditions for additional functionalities
 
         else:
             speak("Sorry Sir, I didn't get that. Can you please repeat?")
 
+
+
+
+
+        
 
